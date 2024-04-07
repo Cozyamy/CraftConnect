e3d713d7d72127fc760fef2da0c111db4cf07114
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-04-07 14:16:52 +0100

    set up redirect to docs on hittting the root request

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,20 @@
+# Importing necessary modules
+import starlette.status as status
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
+# Creating the FastAPI server instance
 server = FastAPI()
+
+
+@server.get("/")
+async def root():
+    """
+    Redirects incoming GET requests to the API documentation page.
+    """
+
+    # Redirecting to the API documentation page using a 302 status code
+    return RedirectResponse(
+        url="/docs",
+        status_code=status.HTTP_302_FOUND,
+    )
